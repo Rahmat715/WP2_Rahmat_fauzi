596cc18a654740f94077abcbf0848529c1803113
Rahmat715 <91046137+Rahmat715@users.noreply.github.com>
2021-09-20 13:26:23 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rahmat Fauzi Widianto saya tinggal di daerah tangerang olah raga yang saya sukai adalah badminton, hobi saya adalah membuat program";
+    }
+}
